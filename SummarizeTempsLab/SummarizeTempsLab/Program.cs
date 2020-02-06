92c545c026ab4eef0a3f0315133426c555c28f2a
cfuqua9 <59981807+cfuqua9@users.noreply.github.com>
2020-02-06 16:30:33 -0600

    Update Program.cs

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -7,7 +7,31 @@ namespace SummarizeTempsLab
     {
         static void Main(string[] args)
         {
-            // temperature data is in temps.txt
+            //temperature data is in temps.txt
+
+            //Write out prompt to the console
+
+            //prompt user for the filename from console
+
+            //Test whether file exists
+
+            //If the file exists...
+
+            //ask the user to enter the temperature threshold
+
+            //Open the file and create StreamReader
+
+            //Read a line into a string variable
+
+            //While the line is not null
+
+            //if the current temperature value >= threshold
+
+            //Increment the above counter by 1
+
+            //Else (temperature is below)
+
+            //Increment the below counter by 1
         }
     }
 }
